06c45be99854f0566a556dc08c80e65a25d9c6e4
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-04-23 12:42:30 +0530

    Home page responsive ~ 80%

--- client/src/components/founders.jsx
+++ client/src/components/founders.jsx
@@ -15,14 +15,14 @@ function founders(){
     <img class="service-image founder-image" src="./images/person.png" alt="Piyush Jaiswal"></img>
     <h2 class="services-heading founder-name">Piyush Jaiswal</h2>
     <h4 class="founder-tagline">Co-founder</h4>
-    <p class="service-para">Expert in developing websites, social media marketing and content creation.</p>
+    <p class="service-para found-para">Expert in developing websites, social media marketing and content creation.</p>
     </div>
 
     <div class="service-box wrapper founder2">
     <img class="service-image founder-image" src="./images/person.png" alt="Digital Buddy"></img>
     <h2 class="services-heading founder-name">Digital Buddy</h2>
     <h4 class="founder-tagline">Co-founder</h4>
-    <p class="service-para">Certified digital marketer, helped many brands to grow their presence from zero to 2M+ following.</p>
+    <p class="service-para found-para">Certified digital marketer, helped many brands to grow their presence from zero to 2M+ following.</p>
     </div>
     </div>
     </div>
